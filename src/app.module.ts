bf6e30cb703d61ab571c600972f1707652f1ad0d
Mustafa <mostafa.wardeh2000@gmail.com>
2024-11-12 16:57:23 +0200

    create Student Entity using TypeORM

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,6 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { StudentsController } from './students/students.controller';
-import { StudentsService } from './students/students.service';
 import { StudentsModule } from './students/students.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
---
+++ src/students/entities/student.entity.ts
@@ -0,0 +1,17 @@
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+
+@Entity()
+export class Student {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column()
+    name: string;
+
+    @Column()
+    age: number;
+
+    @Column('json', { nullable: true }) // nullable mean optional
+    address: string[]
+}
