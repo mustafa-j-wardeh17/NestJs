df3cc12c280fdcdb2eee1b8fcaae729f76bb87f7
Mustafa <mostafa.wardeh2000@gmail.com>
2024-11-12 16:50:51 +0200

    setup typeorm for postgres db taken from docker

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,11 +4,23 @@ import { AppService } from './app.service';
 import { StudentsController } from './students/students.controller';
 import { StudentsService } from './students/students.service';
 import { StudentsModule } from './students/students.module';
-
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   controllers: [AppController],
   providers: [AppService],
-  imports: [StudentsModule],
+  imports: [
+    StudentsModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',  
+      port: 5433,  
+      username: 'postgres',
+      password: 'password',
+      database: 'postgres',
+      autoLoadEntities: true,
+      synchronize: true, // Set to false in production
+    })
+  ],
 })
 export class AppModule {}
