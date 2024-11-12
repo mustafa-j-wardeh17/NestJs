c74bbcce549385d451ce65906f4d5e511f3832bf
Mustafa <mostafa.wardeh2000@gmail.com>
2024-11-12 16:59:02 +0200

    import student feature for students module

--- src/students/students.module.ts
+++ src/students/students.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { StudentsController } from './students.controller';
 import { StudentsService } from './students.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Student } from './entities/student.entity';
 
 @Module({
+    imports: [TypeOrmModule.forFeature([Student])],
     controllers: [StudentsController],
     providers: [StudentsService],
 })
