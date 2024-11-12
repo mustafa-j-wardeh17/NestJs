1eef594617a02036d7b353676c7cb423f23be117
Mustafa <mostafa.wardeh2000@gmail.com>
2024-11-12 10:45:45 +0200

    Types mapping between dtos

--- src/students/dto/update-student.dto/update-student.dto.ts
+++ src/students/dto/update-student.dto/update-student.dto.ts
@@ -1,1 +1,4 @@
-export class UpdateStudentDto {}
+import { PartialType } from "@nestjs/mapped-types";
+import { CreateStudentDto } from "../create-student.dto/create-student.dto";
+
+export class UpdateStudentDto extends PartialType(CreateStudentDto) { }
