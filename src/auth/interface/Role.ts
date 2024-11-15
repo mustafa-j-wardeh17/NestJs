0411169037acceb7e4ce97994cf2ac709880d8e7
mustafawardeh <mostafa.wardeh2000@gmail.com>
2024-11-15 14:18:28 +0300

    dto for authenticate and profile with role

---
+++ src/auth/interface/Role.ts
@@ -0,0 +1,18 @@
+export enum Role {
+    Admin = 'admin',
+    User = 'user'
+}
+
+type User = {
+    id: string;
+    userName: string;
+    password: string;
+    role: Role;
+}
+
+
+// To check if user authenticated
+export interface IAuthenticate {
+    user: User;
+    token: string;
+}
